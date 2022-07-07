e7db6d3d1111d20cb57063f2bc40f4afbecb2dd5
Frederic Seraphine <frederic.seraphine@asp-public.fr>
2022-07-07 12:34:30 +0200

    Corrections du formatage de l'aide

--- README.md
+++ README.md
@@ -15,19 +15,17 @@ exemple : "stop" équivalent à "stop shutdown yes"
 
 help : affiche cette aide.
 
-list : liste toutes les machines ECS du projet.
-list tag=val : liste toutes les machines ECS du projet qui portent le tag avec la valeur précisée.
-
-start : démarre toutes les machines ECS qui portent le tag startup=yes.
-start tag=val : démarre toutes les machines ECS qui portent le tag avec la valeur précisée.
-
-stop : arrête toutes les machines ECS qui portent le tag shutdown=yes.
-start tag=val : arrête toutes les machines ECS qui portent le tag avec la valeur précisée.
+- list : liste toutes les machines ECS du projet.
+- list tag=val : liste toutes les machines ECS du projet qui portent le tag avec la valeur précisée.
+- start : démarre toutes les machines ECS qui portent le tag startup=yes.
+- start tag=val : démarre toutes les machines ECS qui portent le tag avec la valeur précisée.
+- stop : arrête toutes les machines ECS qui portent le tag shutdown=yes.
+- stop tag=val : arrête toutes les machines ECS qui portent le tag avec la valeur précisée.
 
 Vous devez initialiser les variables d'environnement xuivantes :
-FE_REGION : la région Flexible Engine
-FE_DOMAIN_NAME : le nom de votre domain Flexible Engine (page My Credential)
-FE_PROJECT_NAME : le nom de votre projet Flexible Engine (page My Credential)
-FE_PROJECT_ID : l'id de votre projet Flexible Engine (page My Credential)
-FE_USER_NAME : votre nom d'utilisateur (page My Credential)
-FE_USER_PASSWORD : le mot de passe API que vous avez initialisé depuis votre mail "Bienvenue sur la console Flexible Engine"
+- FE_REGION : la région Flexible Engine
+- FE_DOMAIN_NAME : le nom de votre domain Flexible Engine (page My Credential)
+- FE_PROJECT_NAME : le nom de votre projet Flexible Engine (page My Credential)
+- FE_PROJECT_ID : l'id de votre projet Flexible Engine (page My Credential)
+- FE_USER_NAME : votre nom d'utilisateur (page My Credential)
+- FE_USER_PASSWORD : le mot de passe API que vous avez initialisé depuis votre mail "Bienvenue sur la console Flexible Engine"
--- flexeng.py
+++ flexeng.py
@@ -17,23 +17,20 @@ HELP="""flexeng
 
 help : affiche cette aide.
 
-list : liste toutes les machines ECS du projet.
-list tag=val : liste toutes les machines ECS du projet qui portent le tag avec la valeur précisée.
+- list : liste toutes les machines ECS du projet.
+- list tag=val : liste toutes les machines ECS du projet qui portent le tag avec la valeur précisée.
+- start : démarre toutes les machines ECS qui portent le tag startup=yes.
+- start tag=val : démarre toutes les machines ECS qui portent le tag avec la valeur précisée.
+- stop : arrête toutes les machines ECS qui portent le tag shutdown=yes.
+- stop tag=val : arrête toutes les machines ECS qui portent le tag avec la valeur précisée.
 
-start : démarre toutes les machines ECS qui portent le tag startup=yes.
-start tag=val : démarre toutes les machines ECS qui portent le tag avec la valeur précisée.
-
-stop : arrête toutes les machines ECS qui portent le tag shutdown=yes.
-start tag=val : arrête toutes les machines ECS qui portent le tag avec la valeur précisée.
-
-
-Vous devez initialiser les variables d'environnement suivantes :
-FE_REGION : la région Flexible Engine
-FE_DOMAIN_NAME : le nom de votre domain Flexible Engine (page My Credential)
-FE_PROJECT_NAME : le nom de votre projet Flexible Engine (page My Credential)
-FE_PROJECT_ID : l'id de votre projet Flexible Engine (page My Credential)
-FE_USER_NAME : votre nom d'utilisateur (page My Credential)
-FE_USER_PASSWORD : le mot de passe API que vous avez initialisé depuis votre mail "Bienvenue sur la console Flexible Engine"
+Vous devez initialiser les variables d'environnement xuivantes :
+- FE_REGION : la région Flexible Engine
+- FE_DOMAIN_NAME : le nom de votre domain Flexible Engine (page My Credential)
+- FE_PROJECT_NAME : le nom de votre projet Flexible Engine (page My Credential)
+- FE_PROJECT_ID : l'id de votre projet Flexible Engine (page My Credential)
+- FE_USER_NAME : votre nom d'utilisateur (page My Credential)
+- FE_USER_PASSWORD : le mot de passe API que vous avez initialisé depuis votre mail "Bienvenue sur la console Flexible Engine
 """
 
 def get_auth_token():
